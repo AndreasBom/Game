fdbe72668c8d30f756f1e5cb56bc5f648d37f61c
Andreas Bom <andreas@e-avrop.com>
2017-09-19 21:01:21 +0200

    Added actormessages and playeractor

---
+++ Game.ActorModel/Messages/AttackPlayerMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace Game.ActorModel.Messages
+{
+    public class AttackPlayerMessage
+    {
+        public AttackPlayerMessage(string playerName) {
+            PlayerName = playerName;
+        }
+
+        public string PlayerName { get; private set; }
+    }
+}
